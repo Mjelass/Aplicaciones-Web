acfd882f16344fd4fddca7293d92f7b3ad998a73
Mehdi Jelassia <108407452+Mjelass@users.noreply.github.com>
2023-02-01 19:44:31 +0100

    Delete ppvv33 directory

--- ppvv33/config.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-module.exports = {
-  host: 'localhost', // Ordenador que ejecuta el SGBD
-  user: 'root', // Usuario que accede a la BD
-  password: '', // Contraseña con la que se accede a la BD
-  database: 'tareas', // Nombre de la base de datos
-}
